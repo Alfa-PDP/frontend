f67cbe63acf4b322417f42b256decfc45b488992
Constantine <asbest911@gmail.com>
2024-01-24 21:14:55 +0300

    feat: add table to MyTeam page

--- src/pages/MyTeam/MyTeam.tsx
+++ src/pages/MyTeam/MyTeam.tsx
@@ -1,3 +1,15 @@
+import { Typography } from '@alfalab/core-components/typography';
+import TeamTable from '../../components/TeamTable/index';
+import styles from './styles.module.scss';
+
 export default function MyTeam() {
-  return <div>Моя команда</div>;
+  return (
+    <section className={styles.myTeam}>
+      <Typography.Title tag="h1" view="large" weight="bold">
+        Моя команда
+      </Typography.Title>
+
+      <TeamTable />
+    </section>
+  );
 }
