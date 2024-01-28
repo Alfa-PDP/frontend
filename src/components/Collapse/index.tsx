7a3a5df89c9636b34029adbbf0ee2139972a75f0
SenpaiSun <www.maxxi@mail.ru>
2024-01-28 15:49:02 +0600

    fix: fix size textarea

--- src/components/Collapse/index.tsx
+++ src/components/Collapse/index.tsx
@@ -15,7 +15,10 @@ export default function Collapse({
   const textareaRef = useRef<HTMLTextAreaElement>(null);
   const [disabledTextarea, setDisabledTextarea] = useState(true); // Состояние кнопки "Редактировать", если true - textarea открыта и редактируется, если false - закрыта.
   const [textareaValue, setTextareaValue] = useState(false);
-  const employee = false; // Роль руководитель/сотрудник
+  const employee = true; // Роль руководитель/сотрудник
+  const [textareaHeight, setTextareaHeight] = useState<number>(88);
+  const [textAttributesValue, setTextAttributesValue] =
+    useState(textAttributes);
 
   // Определяем, сколько строк в textarea для отключения кнопки "Подробнее"
   useEffect(() => {
@@ -38,32 +41,48 @@ export default function Collapse({
       } else {
         setTextareaValue(false);
       }
+      setTextareaHeight(textareaRef.current.scrollHeight);
     }
   }, [textAttributes]);
 
+  // Изменение размера textarea в зависимости от состояния кнопки "Подробнее"
   useEffect(() => {
     if (textareaRef.current) {
       textareaRef.current.style.height = `${textareaRef.current.scrollHeight - 10}px`;
     }
   }, [stateButtonMore]);
 
+  // Изменение состояния кнопки "Подробнее"
   const handlerButton = () => {
     setStateButtonMore(!stateButtonMore);
   };
 
+  // Изменение состояния кнопки "Редактировать" и изменение активности textarea
   const onClickEdit = () => {
     setStateButtonMore(!stateButtonMore);
     setDisabledTextarea(!disabledTextarea);
   };
 
+  // высота div'a в зависимости от textarea
+  let styleContainer = {};
+
+  if (
+    !textareaValue &&
+    textareaRef.current &&
+    textAttributes &&
+    disabledTextarea
+  ) {
+    styleContainer = { maxHeight: `${textareaHeight + 45}px` };
+  } else if (stateButtonMore && textareaRef.current) {
+    styleContainer = { maxHeight: `${textareaHeight + 75}px` };
+  } else {
+    styleContainer = {};
+  }
+
   return (
     <div
-      className={`${styles.professionalAttributes} ${!textAttributes && styles.professionalAttributes_void} ${stateButtonMore && styles.professionalAttributes_open}`}
-      style={
-        stateButtonMore && textareaRef.current
-          ? { maxHeight: `${textareaRef.current.scrollHeight + 75}px` }
-          : {}
-      }
+      className={`${styles.professionalAttributes} ${!textAttributes ? styles.professionalAttributes_void : styles.professionalAttributes_full} ${stateButtonMore ? styles.professionalAttributes_open : ''}`}
+      style={styleContainer}
     >
       {employee && (
         <button
@@ -83,8 +102,9 @@ export default function Collapse({
         ref={textareaRef}
         className={styles.professionalAttributes__textarea}
         disabled={disabledTextarea}
-        value={textAttributes}
+        value={textAttributesValue}
         placeholder="Не заполнено"
+        onChange={(e) => setTextAttributesValue(e.target.value)}
         style={{
           border: disabledTextarea ? 'none' : '1px solid #E7E8EB',
           backgroundColor: disabledTextarea ? '#fff' : '#F3F4F6',
