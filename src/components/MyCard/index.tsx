63146df5a81763d5697c237afb22142ab49eea15
Constantine <asbest911@gmail.com>
2024-02-05 08:18:45 +0300

    fix: fix Progress bars func

--- src/components/MyCard/index.tsx
+++ src/components/MyCard/index.tsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from 'react';
 import CircularProgressBar from '../CircularProgressBar/index';
 import styles from './styles.module.scss';
 import ProgressBar from '../ProgressBar';
+import { useGetTasksQuery } from '../../store/alfa/alfa.api';
 
 interface MyCardProps {
   data: IndividualPlanWorker;
@@ -34,6 +35,7 @@ export interface Worker {
 }
 
 export interface IndividualPlanWorker extends Worker {
+  user_id: string;
   start_date: string;
   end_date: string;
   year: number;
@@ -48,17 +50,24 @@ const titleProgressBar = [
 
 export default function MyCard({ data }: MyCardProps) {
   const [progressBars, setProgressBars] = useState(titleProgressBar);
+  const { data: tasks } = useGetTasksQuery({
+    user_id: data.user_id,
+    year: data.year,
+  });
+
   useEffect(() => {
     const updatedProgressBars = titleProgressBar.map((bar, index) => {
-      const totalTasks = data.tasks.filter(
-        (task) =>
-          task.task_type === titleProgressBar[index].title.replace(' ', '')
-      ).length;
-      const completedTasks = data.tasks.filter(
-        (task) =>
-          task.status_id === 'c11c2301-5570-4bc2-b7aa-ee0b267db522' &&
-          task.task_type === titleProgressBar[index].title.replace(' ', '')
-      ).length;
+      const totalTasks =
+        tasks?.filter(
+          (task) => task.task_type.name === titleProgressBar[index].title
+        ).length ?? 0;
+
+      const completedTasks =
+        tasks?.filter(
+          (task) =>
+            task.status.slug === 'completed' &&
+            task.task_type.name === titleProgressBar[index].title
+        ).length ?? 0;
 
       return {
         ...bar,
@@ -68,7 +77,7 @@ export default function MyCard({ data }: MyCardProps) {
     });
 
     setProgressBars(updatedProgressBars);
-  }, [data]);
+  }, [tasks]);
 
   return (
     <section className={styles.employee}>
